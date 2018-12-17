0349be674028e232663151bfad37d6abf437f373
CodeWell <ivanastamardjioska@gmail.com>
2018-12-17 17:57:05 +0100

    Initial Commit

---
+++ datePicker/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  datePicker
+//
+//  Created by CodeWell on 10/29/18.
+//  Copyright © 2018 Ivana Stamardjioska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBAction func datePickerChanged(_ sender: Any) {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        
+        let stringDate = dateFormatter.string(from: datePicker.date)
+        dateLabel.text = stringDate
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
